9869b49aebdffa4262489740dfeb4f8f4e14df99
lucas malatchoumy <malatchoumylucas@gmail.com>
2021-09-24 12:06:10 +0200

    feat(main): verification login and password

--- main.sh
+++ main.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+main() {
+  echo -n "Login : "
+  read login
+  echo -n "Password : "
+  read -s password
+  echo ${login} ${password}
+  if [ "$login" != "lucas" ] || [ "$password" != "test1" ]; then
+    echo "Invalid login or password"
+    exit
+  fi
+  echo "You can start";
+    exit
+}
+main
